05d442d865ae0d5e44bc43360727678f4e1e4605
ChinonsoIg <ojononso@yahoo.com>
2022-01-12 13:53:40 +0100

    moved the hassPassword fxnality into the middleware

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 // const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
@@ -29,6 +29,13 @@ const UserSchema = new mongoose.Schema({
     required: [true, 'Please provide a password'],
     minLength: 6,
   }
+})
+
+
+UserSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
 })
 
 
