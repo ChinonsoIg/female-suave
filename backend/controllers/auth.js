5f3297607e44c9ca97cd8b08f68b6ab65b02cb28
ChinonsoIg <ojononso@yahoo.com>
2022-01-12 14:40:08 +0100

    added jwt functionality

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,18 +1,15 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError } = require('../errors')
-const bcrypt = require('bcryptjs');
 
 const register = async (req, res) => {
-  const { name, email, address, password } = req.body;
-
-  // const salt = await bcrypt.genSalt(10);
-  // const hashedPassword = await bcrypt.hash(password, salt)
-
-  // const tempUser = { name, email, address, password: hashedPassword }
-
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user })
+  const token = user.createJWT()
+
+  res
+    .status(StatusCodes.CREATED)
+    .json({ user: { name: user.name } ,  token })
+
 }
 
 
