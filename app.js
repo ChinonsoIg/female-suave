2bbc53bc81120d5a93f440f555ab5bb9b8ee3898
ChinonsoIg <ojononso@yahoo.com>
2023-01-20 22:52:17 +0100

    add security protocols

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 require('dotenv').config()
 require('express-async-errors')
+
+// Extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
-const cors = require('cors');
-
-// newly added
-// var bodyParser = require('body-parser');
-
 
 const app = express();
-
-
 // newly added
 // parse application/x-www-form-urlencoded
 // app.use(bodyParser.urlencoded({ extended: false }))
@@ -17,9 +17,9 @@ const app = express();
 // app.use(bodyParser.json())
 
 // Add cors
-app.use(cors({
-  origin: '*'
-}));
+// app.use(cors({
+//   origin: '*'
+// }));
 
 
 
@@ -33,9 +33,15 @@ const usersRouter = require('./routes/users');
 const categoriesRouter = require('./routes/categories');
 
 // middleware
-
-// if you don't use this, there will be no data in req.body
-app.use(express.json())
+app.use('trust proxy', 1)
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100
+}))
+app.use(express.json()) // if you don't use this, there will be no data in req.body
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use('/api/v1/auth', authRouter)
